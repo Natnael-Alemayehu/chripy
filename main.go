3151f547ff27070a7df867a251818e309e538b30
Natnale Alemayehu <se.natnael.alemayehu@gmail.com>
2026-01-19 14:12:39 +0300

    readiness added but still a dud server

--- main.go
+++ main.go
@@ -1,24 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"time"
 )
 
 func main() {
+	const port = "8080"
 	mux := http.NewServeMux()
 
-	srv := http.Server{
-		Addr:         ":8080",
+	srv := &http.Server{
+		Addr:         ":" + port,
 		Handler:      mux,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 15 * time.Second,
 	}
 
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("./"))))
+
+	mux.HandleFunc("/healthz", handlerReadiness)
+
+	fmt.Println("Serving on port: " + port)
 	err := srv.ListenAndServe()
 	if err != nil {
 		log.Fatalf("Error Server: %v", err)
 	}
+}
 
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
